b7d77c15b8c136102c9f480a3f25874a564e300c
ShadowMeoow <429499757@qq.com>
2025-05-17 09:31:30 +0800

    update progress now
    this is a test

---
+++ src/model/MapModel.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * This class is to record the map of one game. For example:
+ */
+public class MapModel{
+    int[][] matrix;
+    ArrayList<int[][]> history=new ArrayList<>();
+
+    public ArrayList<int[][]> getHistory() {
+        return history;
+    }
+
+    public MapModel(int[][] matrix) {
+        this.matrix = matrix;
+        int[][] initial=new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            initial[i]=Arrays.copyOf(matrix[i],matrix[i].length);
+        }
+        history.add(initial);
+    }
+
+    public int getWidth() {
+        return this.matrix[0].length;
+    }
+
+    public int getHeight() {
+        return this.matrix.length;
+    }
+
+    public int getId(int row, int col) {
+        return matrix[row][col];
+    }
+    public void record(){
+        int[][] current=new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            current[i]=Arrays.copyOf(matrix[i],matrix[i].length);
+        }
+        history.add(current);
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public boolean checkInWidthSize(int col) {
+        return col >= 0 && col < matrix[0].length;
+    }
+
+    public boolean checkInHeightSize(int row) {
+        return row >= 0 && row < matrix.length;
+    }
+    public void reset(){
+        matrix= history.getFirst();
+    }
+    public  void rollback(){
+        matrix= history.get(history.size()-2);
+    }
+}
---
+++ src/view/login/FileController.java
@@ -0,0 +1,71 @@
+package view.login;
+
+import java.io.*;
+import java.util.ArrayList;
+
+//rows are destroyed row //beginning message.
+//comment
+
+
+public class FileController {
+    public static void writer(String toBeWritten) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data.text", true))) {
+            writer.write(toBeWritten);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    //having being tested in 2 different lines
+    public static ArrayList<String> readerAll(String filePath) {
+        ArrayList<String> allData = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                allData.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+        return allData;
+    }
+
+    // check whether the account exist in profile
+    public static boolean checkAccount(String account, String filePath) {
+        ArrayList<String> document = FileController.readerAll(filePath);
+        for (String line : document) {
+            if (line.equals(account)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    return true when correct
+     */
+    // password should be at the second line of account
+    // after checking account, otherwise, return false(when account doesn't exist)
+    public static boolean checkPassword(String password, String account, String filePath) {
+        ArrayList<String> document = FileController.readerAll(filePath);
+        for (int i = 0; i < document.size(); i++) {
+            if (document.get(i).equals(account)) {
+                try {
+                    return document.get(i + 1).equals(password);
+                } catch (Exception e) {
+                    //todo, give a message window
+                    //message destroy
+                    //password can't be empty
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void panelDataSaver(ArrayList<int[][]> arrayList) {
+
+        String s = matrixComputation.historyToString(arrayList);
+        FileController.writer(s);
+    }
+}
